a5efe553261d268467aabce583a83d8ee035b815
Manishashalini1 <129027927+Manishashalini1@users.noreply.github.com>
2023-08-29 23:25:23 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const scanner = require('sonarqube-scanner');
 
 scanner({
-  serverUrl: 'http://13.38.101.1:9000/',
+  serverUrl: 'http://13.36.240.46:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Hospital-management-nodejs',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'password',
+	    'sonar.password': 'admin1',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
